93211b0b253988f780e31ebc52f1f38646a96355
gregk <gregk@evopayments.com>
2020-02-24 15:19:50 -0700

    cleaning data

--- runner.py
+++ runner.py
@@ -3,8 +3,24 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_pickle('data/colorado_accidents')
-print(df.info())
+pd.set_option('display.max_rows', 75)
+pd.set_option('display.max_columns', 75)
+# pd.set_option('display.width', 1000)
+
+df = pd.read_pickle('data/clean_co_accidents')
+
+# df.drop(['Source', 'End_Lat', 'End_Lng', 'Description', 'ID'], axis=1,  inplace=True)
+# df.to_pickle('data/clean_co_accidents')
+
+# print(df.columns)
+df['Start_Time'] = pd.to_datetime(df['Start_Time'])
+df['End_Time'] = pd.to_datetime(df['End_Time'])
+# df['duration'] = (df['End_Time'] - df['Start_Time']).astype('timedelta64[h]')
+df['duration'] = pd.to_datetime((df['End_Time'] - df['Start_Time']).astype('timedelta64[h]'), unit='s')
+
+print(df.head())
+
+# print(df['ID'].head())
 
 '''
 'ID', 'Source', 'TMC', 'Severity', 'Start_Time', 'End_Time', 'Start_Lat', 'Start_Lng', 'End_Lat', 'End_Lng', 
@@ -13,4 +29,12 @@ print(df.info())
 'Visibility(mi)', 'Wind_Direction', 'Wind_Speed(mph)', 'Precipitation(in)', 'Weather_Condition', 'Amenity', 
 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit', 'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming', 
 'Traffic_Signal', 'Turning_Loop', 'Sunrise_Sunset', 'Civil_Twilight', 'Nautical_Twilight', 'Astronomical_Twilight'
+'''
+'''
+'TMC', 'Severity', 'Start_Time', 'End_Time', 'Start_Lat', 'Start_Lng', 'Distance(mi)', 'Number', 'Street', 'Side',
+'City', 'County', 'State', 'Zipcode', 'Country', 'Timezone', 'Airport_Code', 'Weather_Timestamp','Temperature(F)',
+'Wind_Chill(F)', 'Humidity(%)', 'Pressure(in)', 'Visibility(mi)', 'Wind_Direction', 'Wind_Speed(mph)',
+'Precipitation(in)', 'Weather_Condition', 'Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit', 'Railway', 
+'Roundabout', 'Station', 'Stop', 'Traffic_Calming', 'Traffic_Signal', 'Turning_Loop','Sunrise_Sunset', 'Civil_Twilight',
+'Nautical_Twilight', 'Astronomical_Twilight'
 '''
